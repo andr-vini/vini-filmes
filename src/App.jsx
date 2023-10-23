5fcadf717ad6a6f7ee8b244d60ddce69310dd1d2
André Vinicius <andrevinicius.dev@gmail.com>
2023-10-23 18:17:14 -0300

    feat/fallback-route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./Pages/Login"
 import Sign from "./Pages/Sign"
+import Fallback from "./Pages/Fallback"
+import Home from "./Pages/Home"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/sign" element={<Sign/>}/>
+        <Route path="*" element={<Fallback/>}/>
       </Routes>
     </BrowserRouter>
   )
